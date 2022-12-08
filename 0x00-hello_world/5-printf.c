28d6c8b5fbd1c74579d7df0bf4e813c211e63b5c
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-07 23:37:27 -0800

    Adding 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main -Entry point
  *
- * Return: Always 0(success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("With proper grammer,but the outcome is a piece of art,\n");
+	printf("with proper grammer,but the outcome is a piece of art,\n");
 	return (0);
 }
