9aff670f611e9b84ec628e181bd025f442998a27
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-09 12:51:26 -0800

    Adding 3 files

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 /**
- * main -
+ * main - Print all single digit numbers
  *
  * Return: Always 0 (success)
  */
 int main(void)
-{}
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d, i");
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 /**
- * main -
+ * main - Print all single digit numbers of base 10
  *
  * Return: Always 0 (success)
  */
 int main(void)
-{}
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
