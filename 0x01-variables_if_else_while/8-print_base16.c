f7f90ff730ab2ee51046cc3fc0ea00366d67479c
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-09 13:08:15 -0800

    Adding two files

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,4 +5,18 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{}
+{
+	int i;
+
+	for (i = 98; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,4 +5,18 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{}
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
