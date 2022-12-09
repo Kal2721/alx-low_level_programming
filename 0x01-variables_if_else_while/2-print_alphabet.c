b309273a35789b33f7a94ce82de397845cf85c4d
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-09 11:36:46 -0800

    adding 1-last digit file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main -
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{}
