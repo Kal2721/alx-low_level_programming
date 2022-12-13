1047742be58f5e74a730856a42510e2356fdd54d
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-13 03:28:22 -0800

    adding 0-putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main- Print Putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
+	return (0);
+}
