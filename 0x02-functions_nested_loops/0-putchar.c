ddaa0f7d091d34c229794b28199479432be2d83e
Kalkidan <kalkidanteshale6@gmail.com>
2022-12-13 03:32:38 -0800

    fixing 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main- Print Putchar
+ * main - Print Putchar
  *
  * Return: Always 0 (success)
  */
@@ -8,6 +8,7 @@ int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
+
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
