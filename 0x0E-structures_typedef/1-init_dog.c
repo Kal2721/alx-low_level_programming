9bf07391d608f13ab04b5ea41718a88e6b174464
Kalkidan <kalkidanteshale6@gmail.com>
2023-03-21 14:02:26 -0700

    $1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,12 @@
  *
  * Return: Nothing
  */
-void init_dog(struct dog *d, char *name. float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
 		d->name = name;
-		d->age =age;
+		d->age = age;
 		d->owner = owner;
 	}
 }
