8e3bb17878149080e9a65cb33ad663d3c75e5fd7
Kalkidan <kalkidanteshale6@gmail.com>
2023-03-21 13:07:31 -0700

    Adding all files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the file name of the programm
+ *
+ * Return: Always zer
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _HEADER__
+#define _HEADER__
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
