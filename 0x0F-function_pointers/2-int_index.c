fdf10ed0b50f2057e368d1aa38f6ce5f0fbf1f49
Kalkidan <kalkidanteshale6@gmail.com>
2023-03-24 12:48:44 -0700

    Fixing all files

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  * int_index - ...
@@ -19,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 			while(i < size)
 			{
 				if(cmp(array[i]))
-					return(i);
+					return (i);
 
 				i++;
 			}
