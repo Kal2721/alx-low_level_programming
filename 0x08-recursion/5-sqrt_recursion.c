70e224bc00aac0e3bcc9805d312f6637d6d9faeb
Kalkidan <kalkidanteshale6@gmail.com>
2023-06-07 12:02:19 -0700

    $1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,10 +11,17 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+
+/**
+ * _sqrt - ...
+ * @n: ...
+ * @i: ...
+ *
+ * Return: ...
+ */
 int _sqrt(int n, int i)
 {
 	int sqrt = i * i;
-	
 	if (sqrt > n)
 		return (-1);
 
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,25 @@
  */
 int is_prime_number(int n)
 {
-	for (int i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+	return (check_prime(n, 1));
+}
 
-		else
-		{
+/**
+ * check_prime - ...
+ * @n: ...
+ * @i: ...
+ *
+ * Return: ...
+ */
+int check_prime(int n, int i)
+{	
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+			return (0);
+
+	if ((n / i) < i)
 			return (1);
-		}
-	}
+	return (check_prime(n, i + 1));
 }
