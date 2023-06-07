03da6273d04885a826b19bd1c12ce705ca8edc8d
Kalkidan <kalkidanteshale6@gmail.com>
2023-06-07 11:46:34 -0700

    $1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - return square root of a number
+ * @n: the squared number
+ *
+ * Return: the square root result
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+	
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
