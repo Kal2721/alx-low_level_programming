8e302745b67225c94e01f0b711de3ed34fabb9b3
Kal2721 <kalkidanteshale6@gmail.com>
2023-07-05 09:01:17 -0700

    Add all file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
