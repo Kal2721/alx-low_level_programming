5ddf8726bfef65531920d57d5e49f0326d051b4e
Kal2721 <kalkidanteshale6@gmail.com>
2023-07-05 10:11:23 -0700

    Add all file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: the string to be printed 
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	putchar(*s);
+}
