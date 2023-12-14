924df742849e837ee050d221e79c7d8590d5156a
Kalkidan <kalkidanteshale6@gmail.com>
2023-12-14 10:43:51 -0800

    Add all files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char* a = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *a = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	size_t length = strlen(a);
 
 	write(1, a, length);
 
-	return(1);
+	return (1);
 }
