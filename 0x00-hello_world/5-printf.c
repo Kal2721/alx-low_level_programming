91e4539dfe69345f9212e4b23f67e59d61842768
Kalkidan <kalkidanteshale6@gmail.com>
2023-12-14 09:58:44 -0800

    Add all files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
-
+ #include <stdio.h>
+/**
+ * main - prints a sentence
+ *
+ * Return: zero
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
