90bbbb488da6c01536c9211363ce85b45b066a80
Kalkidan <kalkidanteshale6@gmail.com>
2023-12-19 12:01:33 -0800

    Add all files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha -  checks for alphabetic character
+ * c: The alphaber to be checked
+ *
+ * Return: 1 if alpha or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if else (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+	return (0);
+}
