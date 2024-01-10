562ae2ee16368922a49aa8cab21dcc171781723a
Kalkidan <kalkidanteshale6@gmail.com>
2024-01-10 13:47:54 -0800

    Add all files

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,8 +15,9 @@ int is_prime_number(int n)
 }
 
 /**
- * check - to check the numbers parity
+ * check - checks parity
  * @n: number to be checked
+ * @i: iteration number
  *
  * Return: 1 or 0
  */
@@ -25,13 +26,11 @@ int check(int n, int i)
 	if (n <= 1)
 		return (0);
 
-	for (i = 2; i <= n / i; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	if (n % i == 0 && i > 1)
+		return (0);
+
 	if ((n / i) < i)
 		return (1);
 
-	return (0);
+	return (check(n, i + 1));
 }
